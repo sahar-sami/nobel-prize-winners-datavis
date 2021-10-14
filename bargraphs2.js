969069f104d590e119d05410cd3616d06ee4072e
Hannah Daniel <hcd38@cornell.edu>
2021-10-13 22:16:11 -0400

    fix

--- bargraphs2.js
+++ bargraphs2.js
@@ -1,6 +1,6 @@
 function bargraphs2() {
   d3.csv('nobel_prize_by_winner.csv', d3.autoType).then((data) => {
-    data = data.filter(d => d['country'] != null && d['gender'] != null);
+    data = data.filter(d => d['country'] != null && d['gender'] != null && d['gender'] != "org" && (d['country'] == "USA" || d['country'] == "Germany" || d['country'] == "France" || d['country'] == "United Kingdom" || d['country'] == "Switzerland"));
     const svg = d3.select("#vis3").append('svg').attr('width', 725).attr('height', 500);
     const width = svg.attr('width');
     const height = svg.attr('height');
@@ -38,5 +38,48 @@ function bargraphs2() {
       .attr("class", "x gridlines")
       .attr("transform", `translate(${margins.left}, ${chartHeight + margins.top + 10})`)
       .call(bottomGridlines);
+    const labels = svg.append('g')
+      .attr('transform', `translate(0, ${margins.top})`)
+    const botlabels = svg.append('g')
+      .attr('transform', `translate(0, ${margins.right})`)
+    //console.log(Object.keys(winningCountries).length);
+    var subgroups = [male, female]
+    let y = 20
+    let count = 0
+    let axis = 0
+    let xc = 200
+
+    console.log(Object.entries(winningCountries).length);
+    for (const [key, val] of Object.entries(winningCountries)) {
+      labels.append('text')
+        .attr('x', 0)
+        .attr('y', y + 20)
+        .attr('dominant-baseline', 'middle')
+        .attr('font-size', '1.5em')
+        .text(val.country);
+      if (count % 2 == 0) {
+        chartArea.append('rect')
+          .attr('width', numberScale(val.total))
+          .attr('x', 100)
+          .attr('opacity', 0.4)
+          .attr('y', y)
+          .attr('height', 50)
+          .text(val.totals)
+          .style('fill', 'darkblue');
+      }
+      else {
+        chartArea.append('rect')
+          .attr('width', numberScale(val.totals))
+          .attr('x', 100)
+          .attr('opacity', 0.4)
+          .attr('y', y)
+          .attr('height', 50)
+          .style('fill', 'green');
+      }
+      count += 1;
+      y += 20;
+    };
+
+
   })
 }
